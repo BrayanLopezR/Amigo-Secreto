73ed6d00f8dc171f3a77d6d4479ab36513b878b9
Brayan Lopez <bra416@hotmail.com>
2025-01-29 11:24:34 -0600

    Seleccion de amigos aleatorio

--- app.js
+++ app.js
@@ -10,3 +10,26 @@ function agregarAmigos(){
         mostrarListaAmigos();
     }
 }
+
+function mostrarListaAmigos(){
+
+    let listaAmigos=document.getElementById("listaAmigos");
+    listaAmigos.innerHTML="";
+
+    for(let amigo of nombresAmigos){
+        let nuevoElemento=document.createElement("li")
+        nuevoElemento.textContent = amigo;
+        listaAmigos.appendChild(nuevoElemento);
+
+    }
+
+}
+
+function seleccionarAmigoAleatorio(amigos) {
+    if (!Array.isArray(amigos) || amigos.length === 0) {
+      return "No hay amigos en la lista."; 
+    }
+  
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    return amigos[indiceAleatorio];
+  }
