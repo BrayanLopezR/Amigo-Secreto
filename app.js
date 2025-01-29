0fd158bbe530a661f76eede2298e321860c89fc1
Brayan Lopez <bra416@hotmail.com>
2025-01-29 14:24:07 -0600

    Proyecto final

--- app.js
+++ app.js
@@ -1,41 +1,35 @@
+let nombresAmigos = [];
 
-let amigos= []
-
-function agregarAmigos(){
-    nuevoAmigo= document.getElementById("nombreAmigo").value;
+function agregarAmigo() {
+    let nuevoAmigo = document.getElementById("nombresAmigos").value;
     
-    if(nuevoAmigo.trim()!==""){
+    if (nuevoAmigo) {
         nombresAmigos.push(nuevoAmigo);
-        document.getElementById("nombreAmigo").value ="";
+        document.getElementById("nombresAmigos").value = "";
         mostrarListaAmigos();
     }
 }
 
-function mostrarListaAmigos(){
+function mostrarListaAmigos() {
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
 
-    let listaAmigos=document.getElementById("listaAmigos");
-    listaAmigos.innerHTML="";
-
-    for(let amigo of nombresAmigos){
-        let nuevoElemento=document.createElement("li")
+    for (let amigo of nombresAmigos) {
+        let nuevoElemento = document.createElement("li");
         nuevoElemento.textContent = amigo;
         listaAmigos.appendChild(nuevoElemento);
-
     }
-
 }
 
 function sortearAmigo() {
     const amigoAleatorio = seleccionarAmigoAleatorio(nombresAmigos);
-    const resultadoElement = document.getElementById("resultado");
-    resultadoElement.textContent = "El amigo secreto es: " + amigoAleatorio;
+    const resultadoElemento = document.getElementById("resultado");
+    resultadoElemento.textContent = "El amigo secreto es: " + amigoAleatorio;
 }
-
-
 
 function seleccionarAmigoAleatorio(amigos) {
     if (!Array.isArray(amigos) || amigos.length === 0) {
-      return "No hay amigos en la lista."; 
+        return "No hay amigos en la lista."; 
     }
   
     const indiceAleatorio = Math.floor(Math.random() * amigos.length);
